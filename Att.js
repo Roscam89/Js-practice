790000dcef6b548351683c54ea42a503364c478c
max <lil_max2002@yahoo.com>
2022-10-18 21:11:11 -0700

    html and script changes 	modified:   Att.html 	modified:   Att.js

--- Att.js
+++ Att.js
@@ -3,6 +3,11 @@
 const cDisp = document.querySelector(".clockheader");
 const btnStart = document.querySelector(".btnStart");
 const btnStop = document.querySelector(".btnStop");
+const btnLetsGo = document.querySelector(".btnLetsGo");
+const nameactiv = document.querySelector(".nameActiv");
+const activDisplay = document.querySelector(".activType");
+const activDInput = document.querySelector(".input");
+
 let h = 0;
 let m = 0;
 let s = 0;
@@ -31,4 +36,15 @@ btnStart.addEventListener("click", function () {
 
 btnStop.addEventListener("click", function () {
   timeOn = false;
+});
+
+function dispVal() {}
+
+btnLetsGo.addEventListener("click", function () {
+  activDisplay.innerHTML = activDInput.value;
+  nameactiv.style.visibility = "hidden";
+  activDInput.style.visibility = "hidden";
+  btnLetsGo.style.visibility = "hidden";
+  btnStart.style.visibility = "visible";
+  btnStop.style.visibility = "visible";
 });
